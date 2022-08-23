6500194b6438546dffcb8e0dd540ab650c7f6c0e
ME <95096963+MEdma1@users.noreply.github.com>
2022-08-23 10:47:34 +0200

    Update HelloWorld2_WC.js

--- HelloWorld2_WC.js
+++ HelloWorld2_WC.js
@@ -1,6 +1,7 @@
 (function() {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
+	    <h1>DMA</h1>
 	    <div>
 		<button type="button" onclick="document.write(5 + 6)">Try it</button>
 	    </div>
@@ -43,25 +44,7 @@
         onCustomWidgetDestroy() {}
 
 
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-        /*
-        onCustomWidgetResize(width, height){
-            redraw()
-        }
-        */
 
-        redraw() {
-			if (this._tagContainer){
-                this._tagContainer.parentNode.removeChild(this._tagContainer);
-            }
-
-            var shadow = window.getSelection(this._shadowRoot);
-            this._tagContainer = document.createElement(this._tagType);
-            var theText = document.createTextNode(this._tagText);    
-            this._tagContainer.appendChild(theText); 
-            this._shadowRoot.appendChild(this._tagContainer);
-		}
+        redraw() {}
     });
 })();
